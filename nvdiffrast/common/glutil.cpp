277baf10b0509c88e5b9f0ee8ec7e2a9e3b4a817
Clement Fuji Tsang <cfujitsang@nvidia.com>
2022-05-20 20:30:13 -0400

    use GetLastError
    Signed-off-by: Clement Fuji Tsang <cfujitsang@nvidia.com>

--- nvdiffrast/common/glutil.cpp
+++ nvdiffrast/common/glutil.cpp
@@ -57,12 +57,41 @@ static CRITICAL_SECTION getInitializedCriticalSection(void)
 
 static CRITICAL_SECTION s_getProcAddressMutex = getInitializedCriticalSection();
 
+#include "errhandlingapi.h"
+#include "winbase.h"
+
+//Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+std::string GetLastErrorAsString()
+{
+    //Get the error message ID, if any.
+    DWORD errorMessageID = ::GetLastError();
+    if(errorMessageID == 0) {
+        return std::string(); //No error message has been recorded
+    }
+    
+    LPSTR messageBuffer = nullptr;
+
+    //Ask Win32 to give us the string version of that message ID.
+    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+    
+    //Copy the error message into a std::string.
+    std::string message(messageBuffer, size);
+    
+    //Free the Win32's string's buffer.
+    LocalFree(messageBuffer);
+            
+    LOG(INFO) << message;
+}
+
 static void safeGetProcAddress(const char* name, PROC* pfn)
 {
     PROC result = wglGetProcAddress(name);
     if (!result)
     {
         LeaveCriticalSection(&s_getProcAddressMutex); // Prepare for thread exit.
+        GetLastErrorAsString();
         LOG(FATAL) << "wglGetProcAddress() failed for '" << name << "'";
         exit(1); // Should never get here but make sure we exit.
     }
